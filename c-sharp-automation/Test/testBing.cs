a143d590978d3a3f0b95c6fa68e381d6737b2e46
Johanna Santos <johannasabsantos@gmail.com>
2020-12-23 10:34:12 -0300

    add explicit wait function

--- c-sharp-automation/Test/testBing.cs
+++ c-sharp-automation/Test/testBing.cs
@@ -9,29 +9,35 @@ namespace automationExercise
 {
     class searchBing
     {
+        ChromeDriver driver = new ChromeDriver();
+        
+        IWebElement findElementWait(By selector) {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
+            return wait.Until(e => e.FindElement(selector));
+        }
+        
         static void Main(string[] args)
         {
-            ChromeDriver driver = new ChromeDriver();
+            searchBing sb = new searchBing();
             // Navigate to Bing
-            driver.Url = "https://www.bing.com/";
-
+            sb.driver.Url = "https://www.bing.com/";
             // Maximize window
-            var window = driver.Manage().Window;
+            var window = sb.driver.Manage().Window;
             window.Maximize();
 
             // Identify location of search element
-            IWebElement searchInput = driver.FindElement(By.Id("sb_form_q"));
+            IWebElement searchInput = sb.findElementWait(By.Id("sb_form_q"));
             // search for "covid" 
             searchInput.SendKeys("covid" + Keys.Enter);
+            
             // find second result and open the website
-            IWebElement secondResult = driver.FindElement(By.XPath("//ol[@id=\"b_results\"]/li[2]//h2"));
+            IWebElement secondResult = sb.findElementWait(By.XPath("//ol[@id=\"b_results\"]/li[2]//h2"));
             secondResult.Click();
+            
             // Create a text file with the current URL and save it to a txt file
-            File.WriteAllText("test.txt", driver.Url);  
+            File.WriteAllText("test.txt", sb.driver.Url);  
 
-            driver.Close();
-            
-            
+            sb.driver.Close();
         }
     }
 }
